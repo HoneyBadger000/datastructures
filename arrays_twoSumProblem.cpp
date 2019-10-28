199cafe49198971c1b6aa14c51fcc787543d089f
Pravallika <pravallika.swayampu@gmail.com>
2019-10-28 16:24:18 +0100

    "efficient but not working"

--- arrays_twoSumProblem.cpp
+++ arrays_twoSumProblem.cpp
@@ -1,7 +1,8 @@
-#include <array>
+
 #include <algorithm>
 #include <vector>
 #include <iostream>
+#include <array>
 
 using namespace std;
 
@@ -26,19 +27,57 @@ public:
     }
 };
 
+
+class Solution_eff{
+    public:
+    vector<int> twoSum(vector<int>& numset, int target){
+        vector< pair<int, int>> nums2;
+
+        for(int i=0; i<numset.size(); i++)
+        {
+            pair<int,int> temp(numset[i],i);
+            nums2.push_back(temp);
+        }
+
+        // sorting a ector of pairs, sorts it by the first element
+        sort(nums2.begin(),nums2.end());
+
+        int i= 0;
+        int j= nums2.size()-1;
+
+        while(i<j){
+            if(nums2[i].first + nums2[i].first == target){
+                vector<int> t{nums2[i].second, nums2[j].second};
+                return t;
+            }
+            else if(nums2[i].first+nums2[j].first > target){
+                --j;
+            }
+            else
+            {
+                ++i;
+            }                            
+        }
+    }
+};
+
 int main()
 {
-    Solution1 sol;
+    //Solution1 sol;
+
+    Solution_eff sol;
     vector<int> input = {1,6,7,11,67,54,6,3};
 
-    auto found = sol.twoSum(input, 71);
-    cout << "found at : " << found.front() << "," << found.back() << endl;
+    //auto found = sol.twoSum(input, 71);
+    //cout << "found at : " << found.front() << "," << found.back() << endl;
 
-    found = sol.twoSum(input, 57);
+    auto found = sol.twoSum(input, 57);
     cout << "found at : " << found.front() << "," << found.back() << endl;
 
     found = sol.twoSum(input, 73);
     cout << "found at : " << found.front() << "," << found.back() << endl;
+
+
 
     return 0;
 }
